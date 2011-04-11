c0709ca424a9fd2a5868d6fb09021469bf07b80c
Michael Raidel <raidel@induktiv.at>
2011-04-11 16:24:48 +0200

    clean up dependencies

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,10 +6,5 @@ require "rails/test_help"
 
 Rails.backtrace_cleaner.remove_silencers!
 
-# Configure capybara for integration testing
-require "capybara/rails"
-Capybara.default_driver   = :rack_test
-Capybara.default_selector = :css
-
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
